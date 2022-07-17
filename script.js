88e31fe9b479d0d3ebe9d6d0ed6bf8effe1199e9
kunal <thakurrajput98765@gmail.com>
2022-07-17 19:27:43 +0530

    logic complete

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-console.log('hello')
 
-const api = fetch('https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=loki');
 const api_url = fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1');
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 const movieContainer = document.querySelector('.movie-container');
 const allCards = document.querySelector('.cards');
+const search_movie = document.querySelector('.search_movie');
+
+
+
+api_url.then(response => response.json())
+    .then(data => {
+    renderMovie(data);
+})
+
+
+
+
+search_movie.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // cl
+    allCards.innerHTML = '';
+
+    fetchAPI(e.target[0].value);
+
+    // setTimeout(() => {
+    //     allCards.style.opacity = '1';
+    // }, 1000);
+    e.target[0].value = '';
+})
 
 
 const renderMovie = (collection) => {
@@ -18,7 +41,7 @@ const renderMovie = (collection) => {
              </a>
             <div class="info">
                 <h3>${data.title}</h3>
-                <span class="rating">${data.vote_average.toFixed(1)}</span>
+                <span class="rating ${data.vote_average.toFixed(1) >= 8.0 ? 'green' : 'orange'}">${data.vote_average.toFixed(1)}</span>
             </div>
             <div class="overview">
                 <h2>Overview</h2>
@@ -33,17 +56,12 @@ const renderMovie = (collection) => {
 
 }
 
+const fetchAPI = (movie) => {
+    const api = fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=${movie}`);
 
-api.then(response => {
-    console.log(response)
-    return response.json();
-}).then(data => {
-    console.log(data);
-    renderMovie(data);
-})
-
-// api_url.then(response => {
-//     return response.json();
-// }).then(data => {
-//     console.log(data);
-// })
+    api.then(response => response.json())
+        .then(data => {
+        console.log(data);
+        renderMovie(data);
+    })
+}
